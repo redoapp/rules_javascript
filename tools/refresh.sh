31284d5993795c4c2086b24b7f015ca1cf4d4699
Paul Draper <paulddraper@gmail.com>
2026-02-28 19:06:11 -0700

    Use r-tools Bazel preset

--- tools/refresh.sh
+++ tools/refresh.sh
@@ -1,3 +1,3 @@
-"$(rlocation rules_javascript/tools/bazel/bazelrc)"
+"$(rlocation rules_javascript/tools/bazel_/bazelrc)"
+"$(rlocation rules_javascript/tools/bazel_/refresh)"
 "$(rlocation rules_javascript/tools/javascript/js)"
-
